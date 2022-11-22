fbd180b1fa273f4679b430c2e3b8aee946899471
tomananze1997 <78642908+tomananze1997@users.noreply.github.com>
2022-11-22 13:35:17 +0100

    Initial commit

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+export const HelloWorld: FC = () => (
+  <>
+    <h1 className='text-3xl font-bold underline'>Hello!</h1>
+  </>
+);
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { HelloWorld } from 'components';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => (
+  <>
+    <HelloWorld />
+  </>
+);
+
+export default Home;
